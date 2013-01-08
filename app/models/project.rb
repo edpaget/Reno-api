d66b436fa58f4f3d8ca601705fc2d4e8750af4a5
Ed Paget <ed.paget@gmail.com>
2013-01-08 16:44:36 -0600

    Different Logging

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,8 +6,7 @@ class Project < ActiveRecord::Base
   validates :github_repository, :presence => true
 
   def self.from_github_webhook payload
-    puts payload
-    puts 'here'
+    puts payload.class
     project = where("name = :name AND github_repository = :repo_url", 
                     {:name => payload[:repository][:name], 
                      :repo_url => payload[:repository][:url]}).first
