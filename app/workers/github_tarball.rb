de220aacbf690d44d46c71c89570d2952db7b548
Ed Paget <ed.paget@gmail.com>
2013-01-15 14:01:04 -0600

    Fix s3 upload of tarballs

--- app/workers/github_tarball.rb
+++ app/workers/github_tarball.rb
@@ -4,12 +4,11 @@ class GithubTarball
   @queue = :github_tarball
 
   def self.perform user_id, project_id
-    user = User.find user_id
-    project = Project.find project_id
+    user = User.find user_id.to_i
+    project = Project.find project_id.to_i
 
     repo_name = project.name
     git_ref = project.last_commit.git_ref
-
 
     client = Octokit::Client.new :login => user.github_username, :oauth_token => user.oauth_token
     tarball_url = client.archive_link repo_name, :ref => git_ref
@@ -18,7 +17,8 @@ class GithubTarball
 
   def self.download ref, url, repo_name
     download_path = "#{ Rails.root }/tmp/#{ref}.tar.gz"
-    open download_path, 'w' do |out|
+    puts download_path
+    File.open(download_path, 'wb').binmode do |out|
       out.write open(url).read
     end
     upload ref, download_path, repo_name
@@ -26,9 +26,10 @@ class GithubTarball
 
   def self.upload ref, path, repo_name
     s3_client = AWS::S3.new
-    bucket = s3_client.buckets['zookeeper']
+    bucket = s3_client.buckets['ubret']
 
-    file_name = "#{repo_name}/#{ref}.tar.gz"
+    file_name = "zookeeper/#{repo_name}/#{ref}.tar.gz"
     bucket.objects[file_name].write :file => path
+    File.delete path
   end
 end
