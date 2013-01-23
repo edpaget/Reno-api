2ef0e13c4302a50e376b01433d7594d27c2676ec
Ed Paget <ed.paget@gmail.com>
2013-01-23 12:47:25 -0600

    Fix typo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     github_yaml = YAML.load_file("#{ Rails.root }/config/github_api.yml")['production']
-    proivder :github, github_yaml['key'], github_yaml['secret'], scope: 'repo,user'
+    provider :github, github_yaml['key'], github_yaml['secret'], scope: 'repo,user'
   else
     provider :github, ENV['ZOO_BUILD_GITHUB_KEY'], ENV['ZOO_BUILD_GITHUB_SECRET'], scope: 'repo,user'
   end
